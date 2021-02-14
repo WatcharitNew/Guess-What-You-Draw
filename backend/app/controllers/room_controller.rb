04be87d7bb599d72ec2f76465a9bfe5c95e8fc48
dolwijit13 <dol51890@gmail.com>
2021-02-14 18:05:09 +0700

    Prevent same/non username in the same room and init .env file

--- backend/app/controllers/room_controller.rb
+++ backend/app/controllers/room_controller.rb
@@ -9,4 +9,21 @@ class RoomController < ApplicationController
         end
         render json: {room: newRoom}, status: :created
     end
+
+    def join
+        room = params[:room]
+        username = params[:username]
+        roomData = $redis.get(room)
+        if roomData
+            roomData = JSON.parse(roomData)
+            usernames = roomData['usernames']
+            if usernames.include? username
+                render json: {error: 'Duplicate username'}, status: :bad_request
+            else
+                render status: :ok
+            end
+        else
+            render json: {error: 'Room not found'}, status: :bad_request
+        end
+    end
 end
